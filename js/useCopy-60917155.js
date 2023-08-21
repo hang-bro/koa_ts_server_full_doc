c18575f15da52c39775623bf0b197bde4e4f71c1
huangyuhang <772269729@qq.com>
2023-08-21 11:40:54 +0800

    new

--- js/useCopy-60917155.js
+++
@@ -1,1 +0,0 @@
-import{a9 as t}from"./index-7145d906.js";const n=o=>{try{navigator.clipboard.writeText(o).then(()=>t.success("复制成功"))}catch{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t.success("复制成功")}};export{n as u};
---
+++ js/useKeyDown-e25bc02c.js
@@ -0,0 +1,1 @@
+import{s as o,O as e,v as t}from"./index-6ff512aa.js";const u=n=>{o(()=>e(()=>document.onkeydown=n)),t(()=>document.onkeydown=null)};export{u};
